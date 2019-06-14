c82300a51690f17501c73ec3be502b8ec553b52b
maryeddy <maryeddy0@gmail.com>
2019-06-14 10:37:36 -0400

    initial

---
+++ src/main/java/com/petmatch/PetMatch/PetMatchApplication.java
@@ -0,0 +1,13 @@
+package com.petmatch.PetMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetMatchApplication.class, args);
+	}
+
+}
